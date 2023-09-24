0bbace164159ea30ad30543cb61f8c5bf1cfed6d
geokal <kalpgiorgos@gmail.com>
2023-09-24 14:31:05 +0300

    Fixed log message

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ const app = express()
 
 
 const getCurrentUser = ({ headers }) => {
-  return headers['mock-logged-in-as'] ||
-         headers['x-authenticated-userid']
+    return headers['mock-logged-in-as'] ||
+        headers['x-authenticated-userid']
 }
 
 app.get('/stepcounts', (req, res, next) => {
-  console.log(req.headers)
-  const user = getCurrentUser(req)
-  if (!user) {
-    res.status(401).send('Not authorized')
-    return
-  }
-  res.send(data[user] || [])
+    console.log(req.headers)
+    const user = getCurrentUser(req)
+    if (!user) {
+        res.status(401).send('Not authorized')
+        return
+    }
+    res.send(data[user] || [])
 })
 
 const server = https.createServer({ key: SSL_KEY, cert: SSL_CERT }, app)
 server.listen(port, () => {
-  console.log("Server is listening on https://konghq.com:${port}")
+    console.log("Server is listening on https://127.0.0.1:${port}")
 })
 
